86d02872c7b837314d4f95bff59ffd2a953ecaf1
Nnamdi92 <echezonannamdi@gmail.com>
2025-02-02 19:48:01 +0000

    initial commit to the repository

---
+++ SRC/constants/scrapper_constants.py
@@ -0,0 +1,3 @@
+PUNCH_NEWS_URL = "https://punchng.com/topics/news/page"
+
+SUN_NEWS_URL = "https://thesun.ng/category/business/"
---
+++ SRC/database/db_utils.py
@@ -0,0 +1,20 @@
+# This contains utility functions that will help us interactwith the database
+
+from mysql.connector import Error
+
+def create_database(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        print("Database created successfully")
+    except Error as err:
+        print(f"Error: '{err}'")
+ 
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query successful")
+    except Error as err:
+        print(f"Error: '{err}'")
